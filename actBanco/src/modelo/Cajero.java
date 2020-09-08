410a2c2c81e9ce7dd023e553069c8dd2a528def3
pipefake <70596191+pipefake@users.noreply.github.com>
2020-09-07 20:48:17 -0500

    impor cola
    cola

--- actBanco/src/modelo/Cajero.java
+++ actBanco/src/modelo/Cajero.java
@@ -5,10 +5,45 @@
  */
 package modelo;
 
+import core.Base;
+
 /**
  *
  * @author Universidad Felipe
  */
-public class Cajero {
+public class Cajero extends Base {
+
+    @Override
+    public Base copy() {
+        return new Cajero();
+    }
     
+    public boolean estado ()
+    {
+          boolean estado = true;
+         String A = ",";
+         
+         if (A == "Libre")
+         {
+               estado = true;
+         }
+         else if (A != "Libre")
+         {
+             estado = false;
+         }
+        return estado;    
+    }
+    
+    public double promedioTiempo()
+    {
+        double tiempo = 0;
+        double promedio=0.0;
+        double total=0;
+        
+         tiempo = 0+Math.random()*1;
+        
+        promedio = tiempo*100/total;
+        return promedio;
+    }
 }
+
