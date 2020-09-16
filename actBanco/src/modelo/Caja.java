60ee8db77672b5f2cf06d457aa5b360c885a875b
Jonathan Rojas <Jonathan.rojas@uao.edu.co>
2020-09-15 21:08:33 -0500

    encabezados clases actbanco
    jeje

--- actBanco/src/modelo/Caja.java
+++ actBanco/src/modelo/Caja.java
@@ -1,3 +1,17 @@
+/**
+ 
+* Caja
+* @author Jonathan.rojas@uao.edu.co Jonathan Rojas Ibanez 2190039
+ 
+* @author andrea.perez@uao.edu.co Andrea Lucia Perez Marquinez 2190527
+ 
+* @author juan_felipe.jimenez@uao.edu.co Juan Felipe Jimenez Salazar 2190038
+ 
+* @date 03 Septiembre 2020si
+ 
+* @version 1.0
+ 
+*/
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
